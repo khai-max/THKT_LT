fe6da99be2927f4f864defa78645c3801ca43e38
khai-max <nguyentienkhai2910@gmail.com>
2024-11-29 23:39:25 +0700

    Update bai84.cpp

--- bai84.cpp
+++ bai84.cpp
@@ -1,19 +1,52 @@
-#include<stdio.h>
- int main (){
- 	int n;
-	 scanf("%d",&n);
-	 int A[n][n];
+#include <stdio.h>
+#include <math.h> 
+
+void nhap(int A[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]); 
+        } 
+    } 
+}
+
+void xuat(int A[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", A[i][j]); 
+        }
+        printf("\n");
+    }
+} 
+
+float tinhdinhthuc(int A[][100], int n) {
+    float det = 0; 
+    int matrancon[100][100];
+    if (n == 2) {
+        return ((A[0][0] * A[1][1]) - (A[1][0] * A[0][1]));
+    } else {
+        for (int x = 0; x < n; x++) {
+            int sohangi = 0;
+            for (int i = 1; i < n; i++) {
+                int sohangj = 0;
+                for (int j = 0; j < n; j++) {
+                    if (j == x) continue; 
+                    matrancon[sohangi][sohangj] = A[i][j];
+                    sohangj++;
+                }
+                sohangi++;
+            }
+            det += (pow(-1, x) * A[0][x] * tinhdinhthuc(matrancon, n - 1));
+        }
+    }
+    return det;
+}
+
+int main() {
+    int n, A[100][100];
+    scanf("%d", &n);
+    nhap(A, n);
+    xuat(A, n);
+    float det = tinhdinhthuc(A, n);  
+    printf("dinhthuc =  %.2f\n", det);
+    return 0;}
 	 
-	 for (int i=0; i<n;i++){
-	 	for (int j=0; j<n;j++){
-	 		scanf("%d ", &A[i][j]); 
-		 } 
-	 } 
-	for (int i=0; i<n;i++){
-		for (int j=0; j<n;j++){
-			printf("%d ",A[i][j]); 
-		}
-		printf("\n"); 
-	} 
-	
- } 
